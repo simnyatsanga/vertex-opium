483708596381f65a67ee6f43383a62a078bd81d4
simnyatsanga <simnyatsanga@gmail.com>
2015-09-30 16:53:59 -0300

    First commit with skeleton yeoman scaffolded angular application.

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name vertexOpiumApp
+ * @description
+ * # vertexOpiumApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('vertexOpiumApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vertexOpiumApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the vertexOpiumApp
+ */
+angular.module('vertexOpiumApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
