cdbc2f4e84619d09cba8cfa0d8a72151b92e1de9
simnyatsanga <simnyatsanga@gmail.com>
2015-10-04 23:35:39 -0300

    Removing html boiler plate. adding call for google books api and skeleton ui for the resultant books.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,23 @@
  * Controller of the vertexOpiumApp
  */
 angular.module('vertexOpiumApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope, $http) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.stuff = "abc";
+
+    $http.get('https://www.googleapis.com/books/v1/volumes?q=game+engine')
+      .success(function(data, status) {
+          $scope.books = data.items;
+          $scope.numberOfBooks = data.totalItems;
+          $scope.status = status;
+      })
+      .error(function(data, status) {
+          $scope.status = status;
+      });
+
   });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -20,4 +20,19 @@ describe('Controller: MainCtrl', function () {
   it('should attach a list of awesomeThings to the scope', function () {
     expect(MainCtrl.awesomeThings.length).toBe(3);
   });
+
+  it('should have a string in the scope', function() {
+    expect(scope.stuff).toEqual('abc');
+  });
+
+  it('should demostrate an HTTP 200 response', inject(function($httpBackend) {
+    $httpBackend
+      .when('GET', 'https://www.googleapis.com/books/v1/volumes?q=game+engine')
+      .respond(200, {'items': {foo : 'bar'}});
+
+      $httpBackend.flush();
+
+      expect(scope.status).toEqual(200);
+      expect(scope.books).toEqual({foo : 'bar'});
+  }));
 });
